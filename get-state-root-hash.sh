ec7f66173144743e9fb63d8927977b69cad7a22f
kkstas <109525588+kkstas@users.noreply.github.com>
2023-10-15 22:25:29 +0200

    upgrade state-root-hash script

--- get-state-root-hash.sh
+++ get-state-root-hash.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+NODE_ADDRESS="http://148.251.81.170:7777"
+
+printf "\n> casper-client get-state-root-hash\n>\t--node-address $NODE_ADDRESS\n\n"
+
 # Get JSON response
 #
 OUTPUT=$(casper-client get-state-root-hash \
-    --node-address http://148.251.81.170:7777)
+    --node-address "$NODE_ADDRESS")
+
+echo "$OUTPUT"
 
 # Find hash value
 #
@@ -12,7 +18,9 @@ HASH_RAW=$(echo "$OUTPUT" | jq '.result.state_root_hash')
 # Remove double quotes
 HASH=$(echo "$HASH_RAW" | tr -d '"')
 
-echo "$HASH"
+
+printf "======================= state_root_hash ========================\n$HASH\n================================================================\n"
+
 echo "$HASH" | pbcopy
 echo "$HASH" > state-root-hash.txt
 
